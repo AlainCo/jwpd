4ed50151e2699025920e128318341bf1c44d3451
alain coetmeur <alain.coetmeur@gmail.com>
2019-08-26 08:53:33 +0200

    merge

---
+++ jwpd-root/jwpd-interface/src/test/java/fr/alainco/jwpd/JWPDTest.java
@@ -0,0 +1,25 @@
+package fr.alainco.jwpd;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class JWPDTest {
+
+	// https://howtodoinjava.com/junit-5-tutorial/
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
